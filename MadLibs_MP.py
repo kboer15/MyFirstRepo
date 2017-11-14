72adb75fd20e075b96c9d5ed2642e42a4f595bf3
kboer15 <33457248+kboer15@users.noreply.github.com>
2017-11-14 09:44:55 -0500

    Time Change
    When I tried to play the madlib, it would shut down before I got a chance, so I added a time code so that it wouldn't shut down anymore. :)

--- MadLibs_MP.py
+++ MadLibs_MP.py
@@ -1,3 +1,5 @@
+import time
+
 print("Write a name")
 name1 = input()
 
@@ -21,3 +23,5 @@ verb2 = input()
 
 print( "It was a Sunday morning, " + name1 + " decided to go for a walk to see the " + adjective1 + " man " + verbed1 + " in the "
        + noun2 + " to " + verb2 + " with the squids.")
+
+time.sleep(100)
